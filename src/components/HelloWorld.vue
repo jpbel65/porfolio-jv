b29eacd124b1339b0bce09b86cc031f892cc8732
vitrimel@gmail.com <vitrimel@gmail.com>
2020-01-07 21:19:29 -0500

    end

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,8 +3,33 @@
     <h1>CV de Jean-philippe Bélanger</h1>
     <h2>Bienvenue</h2>
     <b-row id="element">
-      <b-col>
-
+      <b-col sm="4">
+        Je suis étudiant en 3eme année en génie logiciel à l'université Laval.
+        Je suis une personne débrouillard, social et investit dans son travail.
+        Je suis passionné de jeu vidéo et de culture vidéo-ludique. Je cherche à
+        agrandir mes connaissances dans le métier de la production de jeu
+        vidéo.<br />
+        <br />
+        Je me spécialise dans la programmation gameplay. Cependant, je
+        m'intéresse aussi la programmation d'outil et en programmation de réseau
+        en ligne.<br />
+        <br />
+        <a
+          href="https://drive.google.com/file/d/1Dhx7zsXDTPVBMs60rUnym5xgp89yPYP6/view?usp=sharing"
+          >Curriculum Vitae</a
+        ><br />
+        <a
+          href="https://drive.google.com/file/d/1bmF34my206LsUx1kE1neelIuMrxWhwCj/view?usp=sharing"
+          >Relever Note Automne 2019</a
+        >
+      </b-col>
+      <b-col sm="8">
+        <img
+          src="../assets/IMG_20191225_150844.jpg"
+          width="400"
+          height="300"
+          style="transform: rotate(90deg)"
+        />
       </b-col>
     </b-row>
   </b-container>
